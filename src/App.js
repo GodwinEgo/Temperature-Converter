828dc84d7b1d1cc3def1e84a5f38c73be2cfe690
GodwinEgo <godwinehile04@gmail.com>
2022-10-11 00:55:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import Calculator from './Components/Calculator/Calculator';
 
 function App ()
 {
   return (
-    <Routes>
-      <Route path='/' element={ <Calculator /> } />
-    </Routes>
+  <Calculator/>
   );
 }
 
